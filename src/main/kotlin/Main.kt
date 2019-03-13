bac217167c7d4218bb908de32e454f0a15d9637a
Matt Arnold <matt@boppl.me>
2019-03-13 22:34:59 +0000

    Extract logic out into RobotController to enable testing

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,37 +1,19 @@
 import command.CommandFactory
-import data.Edge
 
 object Main {
 
-    private lateinit var grid: Grid
-
     private val inputManager = InputManager(CommandFactory())
-    private val edges = mutableListOf<Edge>()
 
     @JvmStatic
     fun main(args: Array<String>) {
 
         val (width, height) = inputManager.getGridSize()
-        grid = Grid(width, height)
+        val grid = Grid(width, height)
+        val controller = RobotController(inputManager, grid)
 
         do {
-            val start = inputManager.getInitialPosition()
-            val commands = inputManager.getCommands()
-
-            var position = start
-            var result: Grid.Result = Grid.Result.Success(start)
-            for (command in commands) {
-                result = grid.transform(position, command, edges)
-                position = result.position
-                if (result is Grid.Result.Lost) {
-                    val edge = Edge(result.position, command)
-                    edges.add(edge)
-                    break
-                }
-            }
-
-            val lost = if (result is Grid.Result.Lost) "LOST" else ""
-            println("${result.position.x} ${result.position.y} ${result.position.direction.name.substring(0, 1)} $lost")
+            val result = controller.controlRobots()
+            println(result)
 
         } while (true)
     }
---
+++ src/main/kotlin/RobotController.kt
@@ -0,0 +1,34 @@
+import data.Edge
+
+class RobotController(
+        private val inputManager: InputManager,
+        private val grid: Grid
+) {
+
+    private val edges = mutableListOf<Edge>()
+
+    fun controlRobots(): String {
+
+        val start = inputManager.getInitialPosition()
+        val commands = inputManager.getCommands()
+
+        var position = start
+        var result: Grid.Result = Grid.Result.Success(start)
+        for (command in commands) {
+            result = grid.transform(position, command, edges)
+            position = result.position
+            if (result is Grid.Result.Lost) {
+                val edge = Edge(result.position, command)
+                edges.add(edge)
+                break
+            }
+        }
+
+        return getResultString(result)
+    }
+
+    private fun getResultString(result: Grid.Result): String {
+        val lost = if (result is Grid.Result.Lost) "LOST" else ""
+        return "${result.position.x} ${result.position.y} ${result.position.direction.name.substring(0, 1)} $lost"
+    }
+}
