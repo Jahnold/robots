b180c7955717e520a950eef3236dac4c1e8afa1a
Matt Arnold <matt@boppl.me>
2019-03-13 22:03:58 +0000

    Add a loop

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -14,24 +14,26 @@ object Main {
         val (width, height) = inputManager.getGridSize()
         grid = Grid(width, height)
 
-        val start = inputManager.getInitialPosition()
-        val commands = inputManager.getCommands()
+        do {
+            val start = inputManager.getInitialPosition()
+            val commands = inputManager.getCommands()
 
-        var position = start
-        var result: Grid.Result = Grid.Result.Success(start)
-        commands.forEach {command ->
-            result = grid.transform(position, command, edges)
-            if (result is Grid.Result.Success) {
-                position = result.position
+            var position = start
+            var result: Grid.Result = Grid.Result.Success(start)
+            commands.forEach { command ->
+                result = grid.transform(position, command, edges)
+                if (result is Grid.Result.Success) {
+                    position = result.position
+                } else {
+                    val edge = Edge(result.position, command)
+                    edges.add(edge)
+                    return@forEach
+                }
             }
-            else {
-                val edge = Edge(result.position, command)
-                edges.add(edge)
-                return@forEach
-            }
-        }
 
-        println("${result.position.x} ${result.position.y} ${result.position.direction.name.substring(0,1)}")
+            println("${result.position.x} ${result.position.y} ${result.position.direction.name.substring(0, 1)}")
+
+        } while (true)
     }
 
 
