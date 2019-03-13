96ef5705121c6f942de0560057add8eeb6bcabb2
Matt Arnold <matt@boppl.me>
2019-03-13 20:56:15 +0000

    create Grid

---
+++ src/main/kotlin/Grid.kt
@@ -0,0 +1,43 @@
+import command.Command
+import data.Edge
+import data.Position
+
+class Grid(private val width: Int, private val height: Int) {
+
+    fun transform(start: Position, command: Command, edges: List<Edge>): Result {
+
+        if (isPreviousLostLocation(start, command, edges)) return Result.Success(start)
+
+        val (direction, movement) = command.transform(start.direction)
+
+        val newX = start.x + movement.x
+        val newY = start.y + movement.y
+        val newPosition = Position(newX, newY, direction)
+
+        return when (isPositionInGrid(newPosition, width, height)) {
+            true -> Result.Success(newPosition)
+            else -> Result.Lost(start)
+        }
+    }
+
+    private fun isPreviousLostLocation(position: Position, command: Command, edges: List<Edge>): Boolean {
+
+        edges.forEach { edge ->
+            if (edge.position == position && edge.command == command) return true
+        }
+        return false
+    }
+
+    private fun isPositionInGrid(position: Position, gridX: Int, gridY: Int): Boolean {
+
+        val isInX = position.x in 0..gridX
+        val isInY = position.y in 0..gridY
+
+        return isInX && isInY
+    }
+
+    sealed class Result {
+        data class Success(val position: Position): Result()
+        data class Lost(val position: Position): Result()
+    }
+}
